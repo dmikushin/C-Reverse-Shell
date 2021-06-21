f2d1f9b1f1bf1b94ec070ffc2926654c4a02a0f6
Dmitry Mikushin <dmitry@kernelgen.org>
2021-06-21 21:38:08 +0200

    Making the socket send/recv loop asynchronous, which finally resolves all issues

--- include/mainloop.h
+++ include/mainloop.h
@@ -1,62 +1,83 @@
-// Keep socket buffer large enough to avoid the hang at:
-// debug1: expecting SSH2_MSG_KEX_ECDH_REPLY
-// Althiugh this problem is attributed to VPN/MTU parameters,
-// here we can run into a similar issue just due to a too
-// small socket buffer.
+// Keep socket buffer large enough
 #define SZBUFFER 65536
 
 static void mainloop(int src, int dst)
 {
-        char input[SZBUFFER], output[SZBUFFER];
+	char input[SZBUFFER], output[SZBUFFER];
 
-        while (1)
-        {
-                ssize_t szinput = recv(src, input, sizeof(input), 0);
-                if (szinput == -1)
+	fd_set fds;
+
+	while (1)
+	{
+		FD_ZERO(&fds);
+		FD_SET(src, &fds);
+		FD_SET(dst, &fds);
+
+		struct timeval timeout;
+		timeout.tv_usec = 10000;
+		int activity = select(src > dst ? src + 1 : dst + 1, &fds, NULL, NULL, &timeout);
+
+		switch (activity)
 		{
-			fprintf(stderr, "Error in recv(src) at %s:%d\n", __FILE__, __LINE__);
+		case -1 :
+			fprintf(stderr, "Error -1 in select() at %s:%d\n", __FILE__, __LINE__);
 			exit(-1);
-		}
-
-		printf("Transferring %zu bytes from src to dst\n", (size_t)szinput);
-
-                while (szinput)
-                {
-                        ssize_t sent = send(dst, input, szinput, 0);
-                        if (sent == -1)
+		case 0 :
+			// TODO Timeout
+			break;
+		default :
+			if (FD_ISSET(src, &fds))
 			{
-				fprintf(stderr, "Error in send(dst) at %s:%d\n", __FILE__, __LINE__);
-				exit(-1);
+				ssize_t szinput = recv(src, input, sizeof(input), 0);
+				if (szinput == -1)
+				{
+					fprintf(stderr, "Error in recv(src) at %s:%d\n", __FILE__, __LINE__);
+					exit(-1);
+				}
+
+				printf("Transferring %zu bytes from src to dst\n", (size_t)szinput);
+
+				while (szinput)
+				{
+					ssize_t sent = send(dst, input, szinput, 0);
+					if (sent == -1)
+					{
+						fprintf(stderr, "Error in send(dst) at %s:%d\n", __FILE__, __LINE__);
+						exit(-1);
+					}
+
+					szinput -= sent;
+				}
 			}
-
-                        szinput -= sent;
-
-                        while (1)
-                        {
-                                ssize_t szoutput = recv(dst, output, sizeof(output), 0);
-                                if (szoutput == -1)
+			else if (FD_ISSET(dst, &fds))
+			{
+				ssize_t szoutput = recv(dst, output, sizeof(output), 0);
+				if (szoutput == -1)
 				{
 					fprintf(stderr, "Error in recv(dst) at %s:%d\n", __FILE__, __LINE__);
 					exit(-1);
 				}
 
-                                if (szoutput == 0) break;
-
 				printf("Transferring %zu bytes from dst to src\n", (size_t)szoutput);
 
-                                while (szoutput)
-                                {
-                                        ssize_t received = send(src, output, szoutput, 0);
-                                        if (received == -1)
+				while (szoutput)
+				{
+					ssize_t received = send(src, output, szoutput, 0);
+					if (received == -1)
 					{
 						fprintf(stderr, "Error in send(src) at %s:%d\n", __FILE__, __LINE__);
 						exit(-1);
 					}
 
-                                        szoutput -= received;
-                                }
-                        }
-                }
-        }
+					szoutput -= received;
+				}
+			}
+			else
+			{
+				fprintf(stderr, "Unknown fd is set, unreachable is executed at %s:%d\n", __FILE__, __LINE__);
+				exit(-1);
+			}
+		}
+	}
 }
 
